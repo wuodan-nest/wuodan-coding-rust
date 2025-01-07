6f3ca62b428f74b69384e81921e4fb05f3171de3
wuodan <jbpanshu@gmail.com>
2025-01-07 19:48:09 +0530

    removed the target folder to save space. build again when needed.

--- rust-exercises/fibonacci/target/debug/deps/fibonacci-2dd1a2818dd44ed2.d
+++
@@ -1,5 +0,0 @@
-/home/wuodan/wuodan-coding-rust/rust-exercises/fibonacci/target/debug/deps/fibonacci-2dd1a2818dd44ed2: src/main.rs
-
-/home/wuodan/wuodan-coding-rust/rust-exercises/fibonacci/target/debug/deps/fibonacci-2dd1a2818dd44ed2.d: src/main.rs
-
-src/main.rs:
--- rust-exercises/fibonacci/target/debug/fibonacci.d
+++
@@ -1,1 +0,0 @@
-/home/wuodan/wuodan-coding-rust/rust-exercises/fibonacci/target/debug/fibonacci: /home/wuodan/wuodan-coding-rust/rust-exercises/fibonacci/src/main.rs
